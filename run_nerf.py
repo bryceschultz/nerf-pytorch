11195df071965f682a5007e2b5801e67a7e6a711
Bryce Schultz <bryceschultz26@gmail.com>
2024-06-23 13:01:25 -0400

    increase training iterations by 1

--- run_nerf.py
+++ run_nerf.py
@@ -702,7 +702,7 @@ def train():
         rays_rgb = torch.Tensor(rays_rgb).to(device)
 
 
-    N_iters = args.training_iterations
+    N_iters = args.training_iterations + 1
     print('Begin')
     print('TRAIN views are', i_train)
     print('TEST views are', i_test)
